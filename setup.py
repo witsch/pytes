5df136d3176ca1e4f02101ad8a4b7a39faf9fd45
Andreas Zeidler <az@zitc.de>
2014-07-29 15:15:55 +0200

    :)

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pytes',
+    version='7',
+    description='keep holger safe!',
+    author='Andreas Zeidler',
+    author_email='az@zitc.de',
+    url='http://pytest.org/',
+)
